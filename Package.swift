0996b17a5ff2fa6f0c256aa3dbf9130ee047229a
Norio Nomura <norio.nomura@gmail.com>
2019-07-21 19:56:00 +0900

    Remove `#if` from `Package.swift`

--- Package.swift
+++ Package.swift
@@ -18,28 +18,15 @@ var package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
-    targets: {
-    #if canImport(Darwin) && !os(tvOS) && !os(watchOS)
-        return [
-            .target(name: "CMachExceptionHandler"),
-            .target(name: "XCTAssertCrash",
-                    dependencies: ["CMachExceptionHandler"]),
-            .testTarget(
-                name: "XCTAssertCrashTests",
-                dependencies: ["XCTAssertCrash"]),
-        ]
-    #else
-        return [
-            .target(name: "XCTAssertCrash"),
-                .testTarget(
-                    name: "XCTAssertCrashTests",
-                    dependencies: ["XCTAssertCrash"]),
-        ]
-    #endif
-
-    }() + [
+    targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "CMachExceptionHandler"),
+        .target(name: "XCTAssertCrash",
+                dependencies: ["CMachExceptionHandler"]),
+        .testTarget(
+            name: "XCTAssertCrashTests",
+            dependencies: ["XCTAssertCrash"]),
     ],
     cLanguageStandard: .gnu99
 )
